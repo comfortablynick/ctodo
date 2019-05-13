85af2ec5719721fd1b0b531c662055b0d370ea89
Nick Murphy <comfortablynick@gmail.com>
2019-05-13 13:14:16 -0500

    Add vscode config; use loguru

--- cmake/Findeasyloggingpp.cmake
+++ cmake/Findeasyloggingpp.cmake
@@ -1,19 +1,52 @@
-include(FetchContent)
-FetchContent_Declare(
-  easyloggingpp
-  GIT_REPOSITORY https://github.com/zuhd-org/easyloggingpp.git
-  GIT_TAG 20fdea6e1b5ae42fb50c9179e332c22242030a38)
-FetchContent_GetProperties(easyloggingpp)
-if(NOT easyloggingpp_POPULATED)
-  FetchContent_Populate(easyloggingpp)
-  message(
-    STATUS "PACKAGE easyloggingpp source dir: ${easyloggingpp_SOURCE_DIR}")
-  message(
-    STATUS "PACKAGE easyloggingpp binary dir: ${easyloggingpp_BINARY_DIR}")
-  # option(build_static_lib "Build easyloggingpp as a static library" ON)
-  # add_subdirectory(${easyloggingpp_SOURCE_DIR} ${easyloggingpp_BINARY_DIR})
-  include_directories("${easyloggingpp_SOURCE_DIR}/src")
-  add_library(easyloggingpp STATIC
-              ${easyloggingpp_SOURCE_DIR}/src/easylogging++.cc)
-  # target_link_options(easyloggingpp PUBLIC -pthread -lpthread -ldl)
+# Finds easylogging++ This will define the following variables
+# EASYLOGGINGPP_FOUND and the following imported target
+# easyloggingpp::easyloggingpp Created by Christian von Arnim (Github @ccvca)
+
+find_library(EASYLOGGINGPP_LIB easyloggingpp)
+
+if(NOT "${EASYLOGGINGPP_LIB}" STREQUAL "EASYLOGGINGPP_LIB-NOTFOUND")
+  message("Found easylogging library: '${EASYLOGGINGPP_LIB}'")
+endif()
+
+find_path(EASYLOGGINGPP_INCLUDE easylogging++.h)
+if(NOT "${EASYLOGGINGPP_INCLUDE}" STREQUAL "EASYLOGGINGPP_INCLUDE-NOTFOUND")
+  message(STATUS "Found easylogging++.h in:'${EASYLOGGINGPP_INCLUDE}' ")
+  set(EASYLOGGINGPP_FOUND 1)
+endif()
+
+if(NOT EASYLOGGINGPP_FOUND)
+  message(STATUS "Could not found easylogging++ locally")
+  message(STATUS "Getting external package easylogging++")
+  include(FetchContent)
+  FetchContent_Declare(
+    easyloggingpp
+    GIT_REPOSITORY https://github.com/zuhd-org/easyloggingpp.git
+    GIT_TAG 20fdea6e1b5ae42fb50c9179e332c22242030a38)
+  FetchContent_GetProperties(easyloggingpp)
+  if(NOT easyloggingpp_POPULATED)
+    FetchContent_Populate(easyloggingpp)
+    message(
+      STATUS "PACKAGE easyloggingpp source dir: ${easyloggingpp_SOURCE_DIR}")
+    message(
+      STATUS "PACKAGE easyloggingpp binary dir: ${easyloggingpp_BINARY_DIR}")
+    # include_directories("${easyloggingpp_SOURCE_DIR}/src")
+    set(EASYLOGGINGPP_INCLUDE ${easyloggingpp_SOURCE_DIR}/src)
+    set(EASYLOGGINGPP_FOUND 1)
+  endif()
+endif()
+
+if(EASYLOGGINGPP_FOUND)
+  add_library(easyloggingpp ${EASYLOGGINGPP_INCLUDE}/easylogging++.cc)
+  target_compile_definitions(easyloggingpp
+                             PUBLIC
+                             ELPP_STL_LOGGING
+                             ELPP_THREAD_SAFE
+                             ELPP_NO_DEFAULT_LOG_FILE
+                             ELPP_USE_STD_THREADING)
+
+  # add_library(easyloggingpp::easyloggingpp IMPORTED INTERFACE)
+  # target_link_libraries(easyloggingpp::easyloggingpp INTERFACE easyloggingpp)
+  target_include_directories(easyloggingpp
+                             INTERFACE ${EASYLOGGINGPP_INCLUDE})
+  add_library(easyloggingpp::easyloggingpp ALIAS easyloggingpp)
 endif()
--- src/main.cc
+++ src/main.cc
@@ -1,10 +1,8 @@
 #include <fstream>
 #include <iostream>
-// #include <loguru.hpp>
-#include "easylogging++.h"
+#include <loguru.hpp>
 #include <regex>
 #include <string>
-INITIALIZE_EASYLOGGINGPP
 
 inline std::string expand_env(std::string text)
 {
@@ -18,17 +16,46 @@ inline std::string expand_env(std::string text)
     return text;
 }
 
+struct termsize
+{
+    unsigned cols, lines;
+};
+
+std::shared_ptr<termsize> getTermSize()
+{
+    auto tsize_t = std::make_shared<termsize>();
+#if defined(TIOCGSIZE)
+    struct ttysize ts;
+    ioctl(STDIN_FILENO, TIOCGSIZE, &ts);
+    tsize_t->cols = ts.ts_cols;
+    tsize_t->lines = ts.ts_lines;
+#elif defined(TIOCGWINSZ)
+    struct winsize ts;
+    ioctl(STDIN_FILENO, TIOCGWINSZ, &ts);
+    tsize_t->cols = ts.ws_col;
+    tsize_t->lines = ts.ws_row;
+#endif
+    return tsize_t;
+}
+
 int main(int argc, char** argv)
 {
-    // loguru::init(argc, argv);
-    START_EASYLOGGINGPP(argc, argv);
-    el::Configurations defaultConf;
-    defaultConf.setToDefault();
-    defaultConf.set(el::Level::Verbose, el::ConfigurationType::Format,
-                    "%level%vlevel|%fbase:%line|%msg");
-    el::Loggers::reconfigureLogger("default", defaultConf);
+    // init loguru
+    loguru::g_colorlogtostderr = true;
+    loguru::g_flush_interval_ms = 100;
+    if (auto tsize = getTermSize(); tsize->cols < 200) {
+        LOG_F(1, "Log output adjusted for term size: %dx%d", tsize->cols, tsize->lines);
+        loguru::g_preamble_thread = false;
+        loguru::g_preamble_date = false;
+        loguru::g_preamble_time = false;
+        if (tsize->cols < 100) {
+            loguru::g_preamble_file = false;
+        }
+    }
+    loguru::init(argc, argv);
+
     auto fpath = expand_env("${HOME}/Dropbox/todo/todo.txt");
-    VLOG(1) << "File path: " << fpath;
+    VLOG_S(1) << "File path: " << fpath;
     std::ifstream file(fpath);
     if (file.is_open()) {
         std::string line;
